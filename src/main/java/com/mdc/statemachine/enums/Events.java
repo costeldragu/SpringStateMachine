36ae1cf5b8d377c9cd7077af3c0711e9a2422ee3
Costel Dragu <dragu.costel@yahoo.com>
2019-11-28 21:31:28 +0200

    My first state machine

---
+++ src/main/java/com/mdc/statemachine/enums/Events.java
@@ -0,0 +1,5 @@
+package com.mdc.statemachine.enums;
+
+public enum Events {
+    E1, E2, E3
+}
---
+++ src/main/java/com/mdc/statemachine/enums/States.java
@@ -0,0 +1,5 @@
+package com.mdc.statemachine.enums;
+
+public enum States {
+    INITIAL_STATE, S1, S2, END_STATE
+}
